e4ca411e67ba83f4486c4ef2f7acdfb93badd74f
13all13ase <q13all13ase@outlook.jp>
2026-07-26 11:33:09 +0900

    計算の呼び出し用コントローラー層を作成

---
+++ src/main/java/com/example/democi/CalcController.java
@@ -0,0 +1,26 @@
+package com.example.democi;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequiredArgsConstructor
+public class CalcController {
+
+    private final Calculator calculator;
+
+    @GetMapping("/plus/{a}/{b}")
+    public int plus(@PathVariable int a, @PathVariable int b) {
+        return calculator.plus(a, b);
+    }
+
+    @GetMapping("/minus/{a}/{b}")
+    public int minus(@PathVariable int a, @PathVariable int b) {
+        return calculator.minus(a, b);
+    }
+
+}
